3522c3efebb5f227e0ef10d2ca1edb32ded5b375
Cuongdzwl <xxxcuongxxx18@gmail.com>
2024-02-23 14:24:43 +0700

    modified:   backend/webAPI/build.ts 	modified:   backend/webAPI/server/routes.ts

--- backend/webAPI/server/routes.ts
+++ backend/webAPI/server/routes.ts
@@ -1,8 +1,8 @@
 import { Application } from 'express';
 import examplesRouter from './api/controllers/examples/router';
-import authRouter from './api/controllers/auth/router';
+// import authRouter from './api/controllers/auth/router';
 
 export default function routes(app: Application): void {
   app.use('/api/v1/examples', examplesRouter);
-  app.use('/api/v1/auth',authRouter)
+  // app.use('/api/v1/auth',authRouter)
 }
