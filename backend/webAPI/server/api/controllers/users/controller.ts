4d70912ddcc326d0936ae43715ecc0f668fefbdd
quainhanmlc <quainhanmlc@gmail.com>
2024-03-14 08:59:52 -0700

    data_4

--- backend/webAPI/server/api/controllers/users/controller.ts
+++ backend/webAPI/server/api/controllers/users/controller.ts
@@ -2,24 +2,24 @@ import ExamplesService from '../../services/users.service';
 import { Request, Response } from 'express';
 import {ISuper} from '../../interfaces/ISuper.interface'
 
-// export class UsersController implements ISuper {
-// async all(_: Request, res: Response): Promise<void> {
-//     const result = await ExamplesService.all();
-//     res.json(result);
-// }
+export class UsersController {
+async all(_: Request, res: Response): Promise<void> {
+    const result = await ExamplesService.all();
+    res.json(result);
+}
 
-//   byId(req: Request, res: Response): void {
-//     const id = Number.parseInt(req.params['id']);
-//     ExamplesService.byId(id).then((r) => {
-//       if (r) res.json(r);
-//       else res.status(404).end();
-//     });
-//   }
+  byId(req: Request, res: Response): void {
+    const id = Number.parseInt(req.params['id']);
+    ExamplesService.byId(id).then((r) => {
+      if (r) res.json(r);
+      else res.status(404).end();
+    });
+  }
 
-//   create(req: Request, res: Response): void {
-//     ExamplesService.create(req.body.name).then((r) =>
-//       res.status(201).location(`/api/v1/examples/${r.id}`).json(r)
-//     );
-//   }
-// }
-// export default new UsersController();
+  create(req: Request, res: Response): void {
+    ExamplesService.create(req.body.name).then((r) =>
+      res.status(201).location(`/api/v1/examples/${r.id}`).json(r)
+    );
+  }
+}
+export default new UsersController();
