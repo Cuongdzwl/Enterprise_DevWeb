b9b38d13a462c8834682f848671f4ac9301be8a8
Cuongdzwl <xxxcuongxxx18@gmail.com>
2024-03-27 10:35:18 +0700

    modified:   server/api/controllers/faculties/router.ts 	modified:   server/api/services/notifications.service.ts

--- backend/webAPI/server/api/controllers/faculties/router.ts
+++ backend/webAPI/server/api/controllers/faculties/router.ts
@@ -3,8 +3,8 @@ import controller from './controller';
 import { authenticateToken, authorizeFaculty } from '../../middlewares/authentication.handler';
 export default express
   .Router()
-  .post('/',authenticateToken, controller.create)
-  .get('/',authenticateToken, controller.all)
-  .get('/:id',authenticateToken,authorizeFaculty, controller.byId)
-  .delete('/:id',authenticateToken, controller.delete)
-  .put('/:id',authenticateToken, controller.update);
+  .post('/', controller.create)
+  .get('/', controller.all)
+  .get('/:id', controller.byId)
+  .delete('/:id', controller.delete)
+  .put('/:id', controller.update);
