bc3f46d3b0bbb6df24331ba5e34e2d93f946df69
Cuongdzwl <xxxcuongxxx18@gmail.com>
2024-04-21 23:32:32 +0700

    fix deploy

--- backend/webAPI/ecosystem.config.js
+++ backend/webAPI/ecosystem.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
     apps: [
       {
@@ -10,7 +12,19 @@ module.exports = {
         instances: 'max',
         exec_mode: 'cluster',
         autorestart: true,
-        max_memory_restart: '512M'
+        max_memory_restart: '512M',
+        env: {
+          NODE_ENV: 'production',
+          OPENAPI_SPEC: process.env.OPENAPI_SPEC,
+          PORT: process.env.PORT,
+          DATABASE_URL: process.env.DATABASE_URL,
+          AZURE_STORAGE_CONNECTION_STRING: process.env.AZURE_STORAGE_CONNECTION_STRING,
+          AZURE_STORAGE_CONTAINER_NAME: process.env.AZURE_STORAGE_CONTAINER_NAME,
+          JWT_SECRET: process.env.JWT_SECRET,
+          SESSION_SECRET: process.env.SESSION_SECRET,
+          NOVU_API_KEY: process.env.NOVU_API_KEY,
+          NOVU_IDENTIFIER: process.env.NOVU_IDENTIFIER,
+        }
       },
     ],
   };
